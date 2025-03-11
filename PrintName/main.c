f2c8c9e220609abe1daa5a1af7094ccef7a6bb20
adamsjo <adamsjo169@gmail.com>
2025-03-10 17:18:21 -0700

    week 1 training

---
+++ PrintName/main.c
@@ -0,0 +1,21 @@
+/*
+Author: John Adams
+Purpose: This program prints out my name on the screen
+Date: 
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+
+    int john = 5; 
+
+    john = 8;
+
+    //this code is displaying my name
+    printf("Hi, my name is John");
+    return 0;
+
+}
---
+++ Project3/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*This program prints the string value Hello World!*/
+
+int main(int argc, char **argv)
+{
+    int total = 0;
+
+    printf("Hello World\n");
+    return 0;
+    
+}
