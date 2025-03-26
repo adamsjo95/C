34171e8b78e7fab9c3622a4e6f9f507af8d17651
John Adams <adamsjo95@icloud.com>
2025-03-25 22:24:14 -0700

    week 3

---
+++ Week3/SizeOfOperator/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Variables of type char occupy %u\n", sizeof(char));
+    printf("Variables of type short occupy %u\n", sizeof(short));
+    printf("Variables of type int occupy %u\n", sizeof(int));
+    printf("Variables of type long occupy %u\n", sizeof(long));
+    printf("Variables of type long long occupy %u\n", sizeof(long long));
+    printf("Variables of type float occupy %u\n", sizeof(float));
+    printf("Variables of type double occupy %u\n", sizeof(double));
+    printf("Variables of type long double occupy %u\n", sizeof(long double));
+    
+
+}
